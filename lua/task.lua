8973b8a00cf24b213581e2c8ec941945b2ff9bd2
Rohit K Viswanath <kvrohit@gmail.com>
2021-10-05 15:30:40 +0530

    Extract common code to functions (#4)

--- lua/task.lua
+++ lua/task.lua
@@ -1,22 +1,27 @@
 local M = {}
+local api = vim.api
+local get_line = api.nvim_get_current_line
+local set_line = api.nvim_set_current_line
+
+local substitute = function(line, old_sym, new_sym, remove_text)
+    replaced, _ = string.gsub(line, old_sym, new_sym, 1)
+    if remove_text then
+        replaced, _ = string.gsub(replaced, remove_text, "", 1)
+    end
+    return replaced
+end
 
 M.toggle = function()
-    local line = vim.api.nvim_get_current_line()
+    local line = get_line()
     if string.find(line, "☐") then
-        replaced, count = string.gsub(line, "☐", "✔", 1)
-        vim.api.nvim_set_current_line(replaced .. os.date(" @done (%d/%m/%Y %X)"))
+        set_line(substitute(line, "☐", "✔") .. os.date(" @done (%d/%m/%Y %X)"))
     elseif string.find(line, "✔") then
-        replaced, count = string.gsub(line, "✔", "☐", 1)
-        replaced, count = string.gsub(replaced, "%s@done.*", "", 1)
-        vim.api.nvim_set_current_line(replaced)
+        set_line(substitute(line, "✔", "☐", "%s@done.*"))
     elseif string.find(line, "✗") then
-        replaced, count = string.gsub(line, "✗", "☐", 1)
-        replaced, count = string.gsub(replaced, "%s@cancelled.*", "", 1)
-        vim.api.nvim_set_current_line(replaced)
+        set_line(substitute(line, "✗", "☐", "%s@cancelled.*"))
     elseif string.find(line, "%w") then
         str = string.sub(line, string.find(line, "%w"))
-        rep, _ = string.gsub(line, "%w", "☐ " .. str, 1)
-        vim.api.nvim_set_current_line(rep)
+        set_line(substitute(line, "%w", "☐ " .. str))
     else
         pos = string.find(line, "%s")
         if pos == nil then 
@@ -24,32 +29,32 @@ M.toggle = function()
             line = " "
         end
         str = string.sub(line, pos)
-        rep, _ = string.gsub(line, "%s", "☐ " .. str, 1)
-        vim.api.nvim_set_current_line(rep)
+        set_line(substitute(line, "%s", "☐ " .. str))
     end
 end
 
 M.undo = function()
-    local line = vim.api.nvim_get_current_line()
-    replaced, count = string.gsub(line, "☐ ", "", 1)
-    replaced, count = string.gsub(replaced, "%s@done.*", "", 1)
-    replaced, count = string.gsub(replaced, "✔ ", "", 1)
-    vim.api.nvim_set_current_line(replaced)
+    set_line(
+        substitute(
+            substitute(
+                substitute(
+                    get_line(), "☐ ", ""
+                ),
+                "✔ ", "", "%s@done.*"
+            ),
+            "✗ ", "", "%s@cancelled.*"
+        )
+    )
 end
 
 M.cancel = function()
-    local line = vim.api.nvim_get_current_line()
+    local line = get_line()
     if string.find(line, "✗") then
-        replaced, count = string.gsub(line, "✗ ", "☐ ", 1)
-        replaced, count = string.gsub(replaced, "%s@cancelled.*", "", 1)
-        vim.api.nvim_set_current_line(replaced)
+        set_line(substitute(line, "✗ ", "☐ ", "%s@cancelled.*"))
     elseif string.find(line, "✔") then
-        replaced, count = string.gsub(line, "✔ ", "✗ ", 1)
-        replaced, count = string.gsub(replaced, "%s@done.*", "", 1)
-        vim.api.nvim_set_current_line(replaced .. os.date(" @cancelled (%d/%m/%Y %X)"))
+        set_line(substitute(line, "✔ ", "✗ ", "%s@done.*") .. os.date(" @cancelled (%d/%m/%Y %X)"))
     elseif string.find(line, "☐") then
-        replaced, count = string.gsub(line, "☐ ", "✗ ", 1)
-        vim.api.nvim_set_current_line(replaced .. os.date(" @cancelled (%d/%m/%Y %X)"))
+        set_line(substitute(line, "☐ ", "✗ ") .. os.date(" @cancelled (%d/%m/%Y %X)"))
     end
 end
 
